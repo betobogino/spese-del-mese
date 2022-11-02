f67767d8f9906986d9bb18e027b19585c6965ef2
betobogino <beto.bogino@gmail.com>
2022-11-02 06:17:18 -0300

    [ADD] Loader

--- src/components/containers/ContainerButton.jsx
+++ src/components/containers/ContainerButton.jsx
@@ -1,22 +1,31 @@
 import { useState, useEffect } from "react";
+import PuffLoader from "react-spinners/PuffLoader";
 
 import Button from "../Button";
 import { getCategories } from "../../services/firebaseCRUD";
 
 const ContainerButton = () => {
     const [buttons, setButtons] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         getCategories()
-            .then(data => setButtons(data))
+            .then(data => {
+                setButtons(data)
+                setLoading(false)
+            })
     }, []);
     
     return (
         <div>
         {
+            loading 
+            ? 
+            <PuffLoader color="#7c36d6" />
+            :
             buttons.map(button => {
                 return <Button key={button.id} id={button.id} icon={button.data.icon} name={button.data.name} amount={button.data.amount}/>
-            })
+            })    
         }
         </div>
     )
