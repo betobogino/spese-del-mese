405fc3a9fd9fcdb1f65863be152aa61e2ecb5414
betobogino <beto.bogino@gmail.com>
2022-11-14 06:29:19 -0300

    Implementacion ReactHookForm

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,42 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import styles from '../styles/Form.module.css';
+import { insertSpent } from '../services/firebaseCRUD';
 
 const Form = ({name, icon}) => {
-  return (
-    <div className={styles.form}>
-        {/* <p className={styles.formTitle}>{name.toUpperCase()}</p> */}
-        <p className={styles.formTitle}>{name}</p>
-        <img className={styles.formIcon} src={`/public/icons/${icon}`} alt="" />
-        <label className={styles.formAmount}>
-            <input type="text" placeholder='Monto'/>
-        </label>
-        <label className={styles.formObservations}>
-            <textarea type="text" cols={"50"} rows={"5"}/>
-        </label>
-        <div className={styles.formControl}>
-          <button>Guardar</button>
-          <Link to="/">
-            <button>Volver</button>
-          </Link>
-        </div>
-    </div>
-  )
+	const { register, handleSubmit, formState:{ errors } } = useForm();
+
+	const pruebaSpent = (event) => {
+		console.log(event)
+	}
+
+	useEffect(() => {	
+
+	}, []);
+
+  	return (
+		<form className={styles.form} onSubmit={handleSubmit(pruebaSpent)}>
+			<input {...register("name")} className={styles.formTitle} defaultValue={name} readOnly={true}/>
+			<img className={styles.formIcon} src={`/public/icons/${icon}`} alt="" />
+			<label className={styles.formAmount}>
+				<input {...register("amount", { required: true }) } type="text" placeholder='Monto'/>
+				<p className={styles.formErrorMsj}>{errors.amount && 'Debe ingresar el monto del gasto'}</p>
+			</label>
+			<input {...register("date", { required: true })} className={styles.formDate} type="date"/>
+			<p className={styles.formErrorMsj}>{errors.date && 'Debe ingresar la fecha del gasto'}</p>
+			<label className={styles.formObservations}>
+				<textarea {...register("observations", { required: true })} type="text" cols={"50"} rows={"5"}/>
+				<p className={styles.formErrorMsj}>{errors.observations && 'Debe ingresar una observación del gasto'}</p>
+			</label>
+			<div className={styles.formControl}>
+				<button>Guardar</button>
+				<Link to="/">
+					<button>Volver</button>
+				</Link>
+			</div>
+		</form>
+  	)
 }
 
 export default Form
--- src/services/firebaseCRUD.js
+++ src/services/firebaseCRUD.js
@@ -1,74 +1,85 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig";
-import { getFirestore, getDocs, collection, getDoc, doc, query, where } from "firebase/firestore";
+import { getFirestore, getDocs, collection, getDoc, doc, query, where, addDoc } from "firebase/firestore";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
 // Obtiene todas las categorias
 export const getCategories = async () => {
-  const categories = await getDocs(query(collection(db, "categories"), where("type", "==", "spent")));
-  const categoriesReturn = [];
+    const categories = await getDocs(query(collection(db, "categories"), where("type", "==", "spent")));
+    const categoriesReturn = [];
 
-  categories.forEach(category => {
-    const buttonAux = {
-        id: category.id,
-        data: category.data()
-    }
-    categoriesReturn.push(buttonAux)
-  })
+    categories.forEach(category => {
+        const buttonAux = {
+            id: category.id,
+            data: category.data()
+        }
+        categoriesReturn.push(buttonAux)
+    })
 
-  return categoriesReturn;
+    return categoriesReturn;
 }  
 // Obtiene una categoria 
 export const getCategory = async (id) => {
-  const category = await getDoc(doc(db, "categories", id));
+    const category = await getDoc(doc(db, "categories", id));
 
-  const categoryReturn = {
-    name: category.data().name, 
-    icon: category.data().icon
-  }
+    const categoryReturn = {
+        name: category.data().name, 
+        icon: category.data().icon
+    }
   
-  return categoryReturn;
+    return categoryReturn;
 }
+
 // Obtiene todos los ingresos
 export const getEntries = async () => {
-  const entries = await getDocs(collection(db, "entries"));
-  const entriesReturn = [];
+    const entries = await getDocs(collection(db, "entries"));
+    const entriesReturn = [];
 
-  entries.forEach(entry => {
-    const entriesAux = {
-        id: entry.id,
-        data: entry.data()
-    }
-    entriesReturn.push(entriesAux)
-  })
+    entries.forEach(entry => {
+        const entriesAux = {
+            id: entry.id,
+            data: entry.data()
+        }
+        entriesReturn.push(entriesAux)
+    })
 
-  return entriesReturn;
+    return entriesReturn;
 }
+
 // Obtiene todos los gastos
 export const getSpents = async () => {
-  const spents = await getDocs(collection(db, "spents"));
-  const spentsReturn = [];
+    const spents = await getDocs(collection(db, "spents"));
+    const spentsReturn = [];
 
-  spents.forEach(spent => {
-    const spentsAux = {
-        id: spent.id,
-        data: spent.data()
-    }
-    spentsReturn.push(spentsAux)
-  })
+    spents.forEach(spent => {
+        const spentsAux = {
+            id: spent.id,
+            data: spent.data()
+        }
+        spentsReturn.push(spentsAux)
+    })
 
-  return spentsReturn;
+    return spentsReturn;
 }
 
 // Obtiene un gasto por name
 export const getSpent = async (name) => {
-  const spents = await getDocs(query(collection(db, "spents"), where("nameCategory", "==", name)));
-  let spentAcumulate = 0;
+    const spents = await getDocs(query(collection(db, "spents"), where("nameCategory", "==", name)));
+    let spentAcumulate = 0;
 
-  spents.forEach(spent => {
-    spentAcumulate += spent.data().amount;
-  })
+    spents.forEach(spent => {
+        spentAcumulate += spent.data().amount;
+    })
   
-  return spentAcumulate;
+    return spentAcumulate;
+}
+
+// Inserta un nuevo gasto
+export const insertSpent = async () => {
+    const docRef = await addDoc(collection(db, "spents"), {
+        
+    });
+    console.log("Document written with ID: ", docRef.id);
 }
